60954bfdab43be9061b755a645d0c43ad116333a
ramaniRK <94068445+ramaniRK@users.noreply.github.com>
2021-11-15 23:46:04 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+package hello;
+public class HelloWorld {
+     public static void main (String args[]) { 
+            System.out.println("Hello World!\n");
+            System.out.println("Welcome");
+            System.out.print("My name is Irfan khan");
+       }
+    }  
+
---
+++ operators.java
@@ -0,0 +1,103 @@
+class Main {
+    public static void main(String[] args) {
+      
+      // declare variables
+      int a = 12, b = 5;
+  
+      // addition operator
+      System.out.println("a + b = " + (a + b));
+  
+      // subtraction operator
+      System.out.println("a - b = " + (a - b));
+  
+      // multiplication operator
+      System.out.println("a * b = " + (a * b));
+  
+      // division operator
+      System.out.println("a / b = " + (a / b));
+  
+      // modulo operator
+      System.out.println("a % b = " + (a % b));
+    }
+  }
+
+  //assingment operator
+  class Main {
+    public static void main(String[] args) {
+      
+      // create variables
+      int a = 4;
+      int var;
+  
+      // assign value using =
+      var = a;
+      System.out.println("var using =: " + var);
+  
+      // assign value using =+
+      var += a;
+      System.out.println("var using +=: " + var);
+  
+      // assign value using =*
+      var *= a;
+      System.out.println("var using *=: " + var);
+    }
+  }
+  //relational operator
+  class Main {
+    public static void main(String[] args) {
+      
+      // create variables
+      int a = 7, b = 11;
+  
+      // value of a and b
+      System.out.println("a is " + a + " and b is " + b);
+  
+      // == operator
+      System.out.println(a == b);  // false
+  
+      // != operator
+      System.out.println(a != b);  // true
+  
+      // > operator
+      System.out.println(a > b);  // false
+  
+      // < operator
+      System.out.println(a < b);  // true
+  
+      // >= operator
+      System.out.println(a >= b);  // false
+  
+      // <= operator
+      System.out.println(a <= b);  // true
+    }
+  }
+  //logical operator
+  class Main {
+    public static void main(String[] args) {
+      
+      // create variables
+      int a = 7, b = 11;
+  
+      // value of a and b
+      System.out.println("a is " + a + " and b is " + b);
+  
+      // == operator
+      System.out.println(a == b);  // false
+  
+      // != operator
+      System.out.println(a != b);  // true
+  
+      // > operator
+      System.out.println(a > b);  // false
+  
+      // < operator
+      System.out.println(a < b);  // true
+  
+      // >= operator
+      System.out.println(a >= b);  // false
+  
+      // <= operator
+      System.out.println(a <= b);  // true
+    }
+  }
+
