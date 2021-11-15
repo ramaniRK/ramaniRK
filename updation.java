fe5474fadca2ae9c8cd9d1f61b35253b8ac6fe4a
ramaniRK <94068445+ramaniRK@users.noreply.github.com>
2021-11-15 23:48:49 +0530

    Add files via upload

---
+++ updation.java
@@ -0,0 +1,22 @@
+public class updation {
+	public static void main (String args[]) {
+		
+		int num = 300;
+		
+		num = num + 50;
+		System.out.println(num);
+		
+		
+		num = num - 60;
+		System.out.println(num); 
+		
+		
+		num = num * 4;
+		System.out.println(num);
+		
+		num = num/10;
+		System.out.println(num); 
+		
+		
+	}
+}
---
+++ while_dowhile.java
@@ -0,0 +1,18 @@
+public class while_dowhile {
+
+	public static void main (String args[]) {
+		
+		int num= 400;
+		while (num<=100) {
+			System.out.println(n);
+			num = num+1;
+		}
+		
+		int num2 = 700;
+		do {
+			System.out.println(num2);
+			num2 = num2+1;
+		} while (num2<=100);
+	
+	}
+}
