70b27f8d37bba517dd5fba9be31c965699e5c0c2
ramaniRK <94068445+ramaniRK@users.noreply.github.com>
2021-11-16 09:59:33 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+public class oddeven {
+
+	public static void main (String args[]) {
+		
+		int i = 432;
+		
+		if(i % 2 ==0) {
+			System.out.print("Even");
+		}else {
+			System.out.print("Odd");
+		}
+	}
+}
---
+++ variable.java
@@ -0,0 +1,42 @@
+package FirstHand_Nov_13;
+
+public class variables {
+
+	 public static void main (String args[]) { 
+		 
+		 // type_of_container name_of_container = value
+		 
+		 
+		 
+		   byte age = 29;
+		   
+		   age = 25;
+		   
+		   short salary = 30000;
+		   int pincode = 515591;
+		   long mobileNo = 90989786759L;
+		   
+		   float percentage = 94.80f;
+		   double randomData = 73826726.978873867d;
+		   
+		   boolean isIndian = true;
+		   
+		   char section = 'D'; // one character within single quote
+		   String name = "irfan"; 
+		   
+		   System.out.println(age);
+		   System.out.println(salary);
+		   System.out.println(pincode);
+		   System.out.println(mobileNo);
+		   
+		   System.out.println(percentage);
+		   System.out.println(randomData);
+		   System.out.println(isIndian);
+		   System.out.println(section);
+		   System.out.println(name);
+		  
+		 
+		   System.out.println(age + " " +  mobileNo + " " + pincode);
+	 }
+}
+
